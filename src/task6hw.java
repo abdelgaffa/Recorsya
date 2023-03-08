b4fac83221bc5c52cdc959040348c57786eee743
abdelgaffa <ahmedabdelhady0246@gmail.com>
2023-03-08 13:35:08 +0300

    finish the task

---
+++ src/task6hw.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class task6hw {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int decimal = input.nextInt();
+        String binary = toBinary(decimal);
+        System.out.println("The binary representation of " + decimal + " is " + binary);
+    }
+
+    public static String toBinary(int decimal) {
+        if (decimal == 0) {
+            return "0";
+        } else if (decimal == 1) {
+            return "1";
+        } else {
+            return toBinary(decimal / 2) + getBinaryDigit(decimal);
+        }
+    }
+
+    public static int getBinaryDigit(int decimal) {
+        if (decimal % 2 == 0) {
+            System.out.print("0");
+            return 0;
+        } else {
+            System.out.print("1");
+            return 1;
+        }
+    }
+}
